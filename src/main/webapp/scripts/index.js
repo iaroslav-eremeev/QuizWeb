27fa2b81f07c903368eb3c3ecda2e574acc9e366
Iaroslav <eremeev.pt@gmail.com>
2023-04-02 12:33:24 +0300

    Add btn-from-internet functionality to index.js

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,42 +1,8 @@
-function load() {
-    {
-        $.ajax({
-            url: 'login',
-            method: "GET",
-            success: [function (users_from_server) {
-                let html_table = "";
-                for (let i = 0; i < users_from_server.length; i++) {
-                    let user = users_from_server[i];
-                    html_table += "<tr>";
-                    html_table += `<td><b>${user.id}</b></td>`;
-                    html_table += `<td>${user.login}</td>`;
-                    html_table += `<td><button class="btn btn-danger" id = "delete-${user.id}">Delete</button></td>`;
-                    html_table += "</tr>";
-                }
-                $('#table-users tbody').html(html_table)
-            }],
-            error: [function () {
-                alert("error!!!")
-            }]
-        })
-    }
-}
-
-load();
-
-$('#table-users').on('click', 'button', function () {
-    let id = $(this).attr('id');
-    $.ajax({
-        url: `login?id=${id}`,
-        type: "DELETE",
-        success: [function () {
-            load();
-        }],
-        error: [function () {
-            alert("error!")
-        }]
-    })
-})
-
-
-
+$('#btn-from-internet').click(function() {
+    // Check if the checkbox is checked
+    var showCorrectAnswers = $('#showCorrectAnswers').prop('checked');
+    // Store the checkbox state in local storage
+    localStorage.setItem('showCorrectAnswers', showCorrectAnswers);
+    // Redirect to loading.html
+    window.location.href = 'loading.html';
+});
