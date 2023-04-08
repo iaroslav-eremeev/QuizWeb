c3f9e25d9e3215fea90838176ce5e58a406ef332
Iaroslav <eremeev.pt@gmail.com>
2023-04-08 18:13:28 +0300

    Update HTML and style, get rid of accordeon

--- target/QuizWeb-1.0-SNAPSHOT/scripts/game.js
+++ target/QuizWeb-1.0-SNAPSHOT/scripts/game.js
@@ -13,41 +13,17 @@ $(document).ready(function() {
     }
 
     function showQuestions() {
-        const accordion = document.querySelector("#accordion");
+        const quizContainer = document.querySelector("#quiz-container");
         const questions = quiz.questions;
         shuffleArray(questions);
         questions.forEach((question, index) => {
             question.answers = [question.correct_answer, ...question.incorrect_answers];
             shuffleArray(question.answers);
 
-            const card = document.createElement("div");
-            const cardHeader = document.createElement("div");
-            const cardBody = document.createElement("div");
-            const questionText = document.createElement("p");
-
-            card.className = "card";
-            cardHeader.className = "card-header";
-            cardHeader.id = `heading-${index}`;
-            cardBody.className = "card-body collapse";
-            cardBody.id = `collapse-${index}`;
-            cardBody.setAttribute("aria-labelledby", `heading-${index}`);
-            cardBody.setAttribute("data-parent", "#accordionExample");
-
-            const button = document.createElement("button");
-            button.className = "btn btn-link";
-            button.type = "button";
-            button.setAttribute("data-toggle", "collapse");
-            button.setAttribute("data-target", `#collapse-${index}`);
-            button.setAttribute("aria-expanded", "false");
-            button.setAttribute("aria-controls", `collapse-${index}`);
-            button.textContent = `Question ${index + 1}`;
-
-            cardHeader.appendChild(button);
-            card.appendChild(cardHeader);
-            card.appendChild(cardBody);
-            accordion.appendChild(card);
-
-            const answerInputs = [];
+            const questionContainer = document.createElement("div");
+            questionContainer.className = "question-container";
+            questionContainer.innerHTML = `<div class="question">${question.question}</div>`;
+            quizContainer.appendChild(questionContainer);
 
             question.answers.forEach((answer) => {
                 const answerLabel = document.createElement("label");
@@ -55,18 +31,14 @@ $(document).ready(function() {
                 answerInput.type = "radio";
                 answerInput.name = `question-${index}`;
                 answerInput.value = String(answer);
-                answerInputs.push(answerInput);
                 answerLabel.textContent = String(answer);
                 answerLabel.className = "answer-label";
-                cardBody.appendChild(answerLabel);
-                cardBody.appendChild(answerInput);
+                questionContainer.appendChild(answerInput);
+                questionContainer.appendChild(answerLabel);
             });
-
-            questionText.textContent = question.question;
-            questionText.className = "question-text";
-            cardBody.insertBefore(questionText, answerInputs[0]);
         });
     }
-    console.log(quiz);
+
+
     showQuestions();
 });
